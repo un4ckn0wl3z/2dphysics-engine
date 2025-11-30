1aaf5ac92492240a8346ced3153bbb1ba12a002a
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-11-30 17:49:29 +0700

    feat: limit screen boundaries for particles

--- 2dphysics-engine/Application.cpp
+++ 2dphysics-engine/Application.cpp
@@ -9,6 +9,7 @@ bool Application::IsRunning() {
 void Application::Setup() {
     m_running = Graphics::OpenWindow();
     m_particle = new Particle(50, 100, 1.0);
+    m_particle->radius = 4;
 }
 
 
@@ -39,7 +40,7 @@ void Application::Update() {
     // calculate deltatime
     float deltatime = (SDL_GetTicks() - timePreviousFrame) / 1000.0f;
 
-    // deltatime protection
+    // deltatime protection for debug purpose
     //if (deltatime > 0.016) {
     //    deltatime = 0.016;
     //}
@@ -47,17 +48,39 @@ void Application::Update() {
     timePreviousFrame = SDL_GetTicks();
 
     // update logic
-    m_particle->acceleration = Vec2(0.0, 9.8 * PIXELS_PER_METER);
+    m_particle->acceleration = Vec2(2.0 * PIXELS_PER_METER, 9.8 * PIXELS_PER_METER);
 
     // apply acceleration and velocity
     m_particle->velocity += m_particle->acceleration * deltatime;
     m_particle->position += m_particle->velocity * deltatime;
+
+    // limit boundaries
+    if (m_particle->position.x - m_particle->radius <= 0) {
+        m_particle->position.x = m_particle->radius;
+        m_particle->velocity.x *= -1.0;
+    }
+    else if (m_particle->position.x + m_particle->radius >= Graphics::Width()) {
+        m_particle->position.x = Graphics::Width() - m_particle->radius;
+        m_particle->velocity.x *= -1.0;
+    }
+
+    if (m_particle->position.y - m_particle->radius <= 0) {
+        m_particle->position.y = m_particle->radius;
+        m_particle->velocity.y *= -1.0;
+    }
+    else if (m_particle->position.y + m_particle->radius >= Graphics::Height()) {
+        m_particle->position.y = Graphics::Height() - m_particle->radius;
+        m_particle->velocity.y *= -1.0;
+    }
+
+
+
 }
 
 
 void Application::Render() {
     Graphics::ClearScreen(0xFF056263);
-    Graphics::DrawFillCircle(m_particle->position.x, m_particle->position.y, 4, 0xFFFFFFFF);
+    Graphics::DrawFillCircle(m_particle->position.x, m_particle->position.y, m_particle->radius, 0xFFFFFFFF);
     Graphics::RenderFrame();
 }
 
--- 2dphysics-engine/Particle.h
+++ 2dphysics-engine/Particle.h
@@ -2,6 +2,9 @@
 #include "Vec2.h"
 
 struct Particle {
+
+	int radius;
+
 	Vec2 position;
 	Vec2 velocity;
 	Vec2 acceleration;
