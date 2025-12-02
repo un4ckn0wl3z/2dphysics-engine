c7560aade6314df4a93027a75114e66d59aa9642
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-12-02 14:48:59 +0700

    feat: play with friction force

--- 2dphysics-engine/Application.cpp
+++ 2dphysics-engine/Application.cpp
@@ -13,14 +13,14 @@ void Application::Setup() {
     //smallBall->radius = 4;
     //m_particles.push_back(smallBall);
 
-    //Particle* bigBall = new Particle(50, 100, 3.0);
-    //bigBall->radius = 12;
-    //m_particles.push_back(bigBall);
+    Particle* bigBall = new Particle(50, 100, 3.0);
+    bigBall->radius = 12;
+    m_particles.push_back(bigBall);
 
-    m_liquid.x = 0;
-    m_liquid.y = Graphics::Height() / 2;
-    m_liquid.w = Graphics::Width();
-    m_liquid.h = Graphics::Height() / 2;
+    //m_liquid.x = 0;
+    //m_liquid.y = Graphics::Height() / 2;
+    //m_liquid.w = Graphics::Width();
+    //m_liquid.h = Graphics::Height() / 2;
 
 }
 
@@ -54,13 +54,34 @@ void Application::Input() {
                 if (event.key.keysym.sym == SDLK_a)
                     m_pushForce.x = 0;
                 break;
+            //case SDL_MOUSEBUTTONDOWN:
+            //    if (event.button.button == SDL_BUTTON_LEFT) {
+            //        int x, y;
+            //        SDL_GetMouseState(&x, &y);
+            //        Particle* particle = new Particle(x, y, 1.0);
+            //        particle->radius = 5;
+            //        m_particles.push_back(particle);
+            //    }
+            //    break;
+            case SDL_MOUSEMOTION:
+                m_mouseCursor.x = event.motion.x;
+                m_mouseCursor.y = event.motion.y;
+                break;
             case SDL_MOUSEBUTTONDOWN:
-                if (event.button.button == SDL_BUTTON_LEFT) {
+                if (!m_leftMouseButtonDown && event.button.button == SDL_BUTTON_LEFT) {
+                    m_leftMouseButtonDown = true;
                     int x, y;
                     SDL_GetMouseState(&x, &y);
-                    Particle* particle = new Particle(x, y, 1.0);
-                    particle->radius = 5;
-                    m_particles.push_back(particle);
+                    m_mouseCursor.x = x;
+                    m_mouseCursor.y = y;
+                }
+                break;
+            case SDL_MOUSEBUTTONUP:
+                if (m_leftMouseButtonDown && event.button.button == SDL_BUTTON_LEFT) {
+                    m_leftMouseButtonDown = false;
+                    Vec2 impulseDirection = (m_particles[0]->position - m_mouseCursor).UnitVector();
+                    float impulseMagnitude = (m_particles[0]->position - m_mouseCursor).Magnitude() * 5.0;
+                    m_particles[0]->velocity = impulseDirection * impulseMagnitude;
                 }
                 break;
         }
@@ -145,6 +166,7 @@ void Application::Update() {
 void Application::Render() {
     Graphics::ClearScreen(0xFF056263);
 
+
     // draw liquid
     //Graphics::DrawFillRect(
     //    m_liquid.x + m_liquid.w / 2,
@@ -156,6 +178,10 @@ void Application::Render() {
 
     for (auto particle : m_particles) {
         Graphics::DrawFillCircle(particle->position.x, particle->position.y, particle->radius, 0xFFFFFFFF);
+    
+        if (m_leftMouseButtonDown) {
+            Graphics::DrawLine(particle->position.x, particle->position.y, m_mouseCursor.x, m_mouseCursor.y, 0xFF0000FF);
+        }
     }
 
     Graphics::RenderFrame();
--- 2dphysics-engine/Application.h
+++ 2dphysics-engine/Application.h
@@ -9,8 +9,9 @@ class Application {
         bool m_running = false;
         std::vector<Particle*> m_particles;
         Vec2 m_pushForce = Vec2(0, 0);
-        
         SDL_Rect m_liquid;
+        Vec2 m_mouseCursor = Vec2(0, 0);
+        bool m_leftMouseButtonDown = false;
 
     public:
         Application() = default;
