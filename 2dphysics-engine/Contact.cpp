cf5e07caba7375f02d7d7699afb21de0c8607758
un4ckn0wl3z <gh0stsync.rt@gmail.com>
2025-12-07 11:38:30 +0700

    feat: revolve collider with projection

--- 2dphysics-engine/Contact.cpp
+++ 2dphysics-engine/Contact.cpp
@@ -1,1 +1,10 @@
 #include "Contact.h"
+
+void Contact::ResolvePenetration()
+{
+	float da = (depth * b->mass) / (a->mass + b->mass);
+	float db = (depth * a->mass) / (a->mass + b->mass);
+
+	a->position -= normal * da;
+	b->position += normal * db;
+}
